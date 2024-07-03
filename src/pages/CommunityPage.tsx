e454a0ba515ed770bdaa28173d9b4d77469041cd
youable <youable.framios@gmail.com>
2024-07-03 16:27:30 +0900

    add: 커뮤니티 페이지 추가

---
+++ src/pages/CommunityPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const CommunityPage = () => {
+    return (
+        <div className="home-container">
+            <Navbar/>
+            <p>contents</p>
+        </div>
+    );
+};
+
+export default CommunityPage;
